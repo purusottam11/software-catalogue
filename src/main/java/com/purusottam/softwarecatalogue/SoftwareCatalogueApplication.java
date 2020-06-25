a997d28bbfb44e022fdac27f8c95b173173e9064
purusottam11 <ppurusottam1@gmail.com>
2020-06-25 14:00:13 +0530

    code clean up

---
+++ src/main/java/com/purusottam/softwarecatalogue/SoftwareCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.purusottam.softwarecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purusottam/softwarecatalogue/SoftwareCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purusottam.softwarecatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
