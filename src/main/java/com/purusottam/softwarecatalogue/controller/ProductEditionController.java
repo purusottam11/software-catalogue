1edf706269bbe52e8bd070432efb4083ccc6592a
Purusottam Mohapatra <35234210+purusottam11@users.noreply.github.com>
2020-09-14 13:21:30 +0530

    product edition finished

--- src/main/java/com/purusottam/softwarecatalogue/controller/ProductEditionController.java
+++ src/main/java/com/purusottam/softwarecatalogue/controller/ProductEditionController.java
@@ -2,12 +2,12 @@ package com.purusottam.softwarecatalogue.controller;
 
 import com.purusottam.softwarecatalogue.bean.ProductEditionBean;
 import com.purusottam.softwarecatalogue.exception.BusinessException;
+import com.purusottam.softwarecatalogue.model.ProductEdition;
 import com.purusottam.softwarecatalogue.service.ProductEditionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/productEdition")
@@ -25,5 +25,32 @@ public class ProductEditionController {
         }
     }
 
+
+    @PutMapping("/updateProductEdition/{id}")
+    public ProductEditionBean updateProductEdition(@RequestBody ProductEditionBean productEditionBean, @PathVariable Long id) {
+        try {
+            return productEditionService.updateProductEdition(productEditionBean, id);
+        } catch (Exception e) {
+            throw new BusinessException(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getProductEdition/{id}")
+    public ProductEditionBean getProductEdition(@PathVariable Long id) {
+        try {
+            return productEditionService.getEdition(id);
+        } catch (Exception e) {
+            throw new BusinessException(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getEditionsByProductIdAndVersionId/{productId}/{versionId}")
+    List<ProductEditionBean> getEditionsByProductIdAndVersionId(@PathVariable Long productId, @PathVariable Long versionId) {
+        try {
+            return productEditionService.getEditionsByProductIdAndVersionId(productId, versionId);
+        } catch (Exception e) {
+            throw new BusinessException(e.getMessage());
+        }
+    }
 
 }
